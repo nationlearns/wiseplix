dc912b2c3ff82179875c680a4d6ff08790cb65e9
unknown <aquib_j@yahoo.com>
2024-09-22 00:29:33 +0530

    Password Reset Fields Updated

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,106 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+<!doctype html>
+<html lang="en">
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <link rel="stylesheet" href="{{ asset('login/fonts/icomoon/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('login/css/owl.carousel.min.css') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('login/css/bootstrap.min.css') }}">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('login/css/style.css') }}">
+
+    <title>Register</title>
+</head>
+
+<body>
+
+
+
+    <div class="content py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 contents">
+
+                    @if ($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                    <div class="row justify-content-center">
+                        <a href="https://wiseplix.com/" class="text-center">
+                            <img src="{{asset('images/logo/wiseplix-logo.png')}}" alt="WisePlix" width="200px" class="mb-4">
+                          </a>
+                        <div class="col-md-12">
+                            <div class="form-block">
+                                <h3 class="mb-4">Reset Password</h3>
+
+                                <!-- Session Status -->
+                                <x-auth-session-status class="mb-4 text-success" :status="session('status')" />
+
+                                    <form method="POST" action="{{ route('password.store') }}">
+                                        @csrf
+
+                                        <!-- Password Reset Token -->
+                                        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                        <!-- Email Address -->
+                                        <div>
+                                            <x-input-label for="email" :value="__('Email')" />
+                                            <x-text-input id="email" class="form-control border p-2" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+
+                                        <!-- Password -->
+                                        <div class="mt-4">
+                                            <x-input-label for="password" :value="__('Password')" />
+                                            <x-text-input id="password" class="form-control border p-2" type="password" name="password" required autocomplete="new-password" />
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+
+                                        <!-- Confirm Password -->
+                                        <div class="mt-4">
+                                            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                                            <x-text-input id="password_confirmation" class="form-control border p-2"
+                                                                type="password"
+                                                                name="password_confirmation" required autocomplete="new-password" />
+
+                                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                        </div>
+
+                                        <div class="flex items-center justify-end mt-4">
+                                            <x-primary-button>
+                                                {{ __('Reset Password') }}
+                                            </x-primary-button>
+                                        </div>
+                                    </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+    </div>
+    <script src="{{ asset('login/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('login/js/popper.min.js') }}"></script>
+    <script src="{{ asset('login/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('login/js/main.js') }}"></script>
+</body>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+</html>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
