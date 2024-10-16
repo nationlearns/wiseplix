5a3bff2ae51fef1b64e93e7ace16ab4a6aabf0ad
unknown <aquib_j@yahoo.com>
2024-10-16 10:17:55 +0530

    Email Verification UI update

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,31 +1,99 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-    </div>
 
-    @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-        </div>
-    @endif
 
-    <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
-            @csrf
+<!doctype html>
+<html lang="en">
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('login/fonts/icomoon/style.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('login/css/owl.carousel.min.css') }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('login/css/bootstrap.min.css') }}">
+
+    <!-- Style -->
+    <link rel="stylesheet" href="{{ asset('login/css/style.css') }}">
+
+    <title>Register</title>
+</head>
+
+<body>
+
+
+
+    <div class="content py-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 contents">
+
+                    <div class="card">
+                        <div class="card-header">
+                            Verify Email
+                        </div>
+
+                        <div class="card-body">
+                            @if ($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+
+                            <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                            </div>
+
+                            @if (session('status') == 'verification-link-sent')
+                                <div class="mb-4 font-medium text-sm text-success">
+                                    {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                                </div>
+                            @endif
+                        </div>
+                        
+                        <div class="card-footer">
+                            <div class="d-flex align-items-center" style="gap: 1rem">
+                                <form method="POST" action="{{ route('verification.send') }}">
+                                    @csrf
+        
+                                    <div>
+                                        <x-primary-button  class="btn btn-dark">
+                                            {{ __('Resend Verification Email') }}
+                                        </x-primary-button>
+                                    </div>
+                                </form>
+        
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+        
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Log Out') }}
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+   
+
+                </div>
+
             </div>
-        </form>
-
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-
-            <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
-            </button>
-        </form>
+        </div>
     </div>
-</x-guest-layout>
+    <script src="{{ asset('login/js/jquery-3.3.1.min.js') }}"></script>
+    <script src="{{ asset('login/js/popper.min.js') }}"></script>
+    <script src="{{ asset('login/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('login/js/main.js') }}"></script>
+</body>
+
+</html>
+
+
